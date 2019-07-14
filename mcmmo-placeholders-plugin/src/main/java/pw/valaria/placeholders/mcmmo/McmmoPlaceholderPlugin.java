cba66f1a8bb8190257daa2cf70cb634f7da06df0
Shane Freeder <theboyetronic@gmail.com>
2019-07-15 00:46:45 +0100

    remove accidental test

--- mcmmo-placeholders-plugin/src/main/java/pw/valaria/placeholders/mcmmo/McmmoPlaceholderPlugin.java
+++ mcmmo-placeholders-plugin/src/main/java/pw/valaria/placeholders/mcmmo/McmmoPlaceholderPlugin.java
@@ -1,15 +1,8 @@
 package pw.valaria.placeholders.mcmmo;
 
-import org.bukkit.Bukkit;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.slf4j.Logger;
 
-import me.clip.placeholderapi.PlaceholderAPI;
-import me.clip.placeholderapi.PlaceholderAPIPlugin;
 import pw.valaria.placeholders.mcmmo.bridge.McmmoBridge;
 import pw.valaria.placeholders.mcmmo.bridge.v2_1.McmmoBridge21;
 
@@ -20,18 +13,6 @@ public class McmmoPlaceholderPlugin extends JavaPlugin implements Listener {
     public void onEnable() {
         // TODO: Implement actual abstraction handling
         bridge.init();
-        Bukkit.getPluginManager().registerEvents(this, this);
-
-    }
-
-
-    @EventHandler
-    public void onJoin(PlayerJoinEvent event) {
-        Bukkit.getScheduler().runTaskLater(this, (task) -> {
-            Bukkit.getPlayer(event.getPlayer().getUniqueId()).sendMessage(PlaceholderAPI.setPlaceholders(event.getPlayer(), "excav: %mCmmo_level_excavatioN% : %mcmmo_xp_remaining_excavation% : %mcmmo_xp_needed_excavation%"));
-        }, 5 * 20);
-
-
     }
 
 }
